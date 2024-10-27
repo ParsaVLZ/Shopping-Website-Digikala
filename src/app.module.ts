25c72a7463468ff6f2e51f1412c17cb33308af75
Parsa Valizadeh <parsavalizadeh4@gmail.com>
2024-10-27 12:12:03 +0330

    first

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import {Module} from "@nestjs/common";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import { ProductModule } from "./modules/product/product.module";
+import { DiscountModule } from "./modules/discount/discount.module";
+import { BasketModule } from "./modules/basket/basket.module";
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      port: 3306,
+      username: "root",
+      password: "",
+      database: "digikala",
+      autoLoadEntities: false,
+      synchronize: true,
+      entities: [
+        "dist/**/**/**/*.entity{.ts,.js}",
+        "dist/**/**/*.entity{.ts,.js}",
+      ],
+    }),
+    ProductModule,
+    DiscountModule,
+    BasketModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
